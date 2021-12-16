30ce161c328b0804252362c4afea36c3c86ab423
HassanHamdanDev <hassanhamdandev@gmail.com>
2021-12-16 14:51:11 +0200

    Add

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -26,8 +26,13 @@ authRouter.post('/signin', basicAuth, (req, res, next) => {
     user: req.user,
     token: req.user.token
   };
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS");
+
   res.status(200).json(user);
 });
 
